6cc72bf7441c3cdbb40d46ccca293e37e8bcfc24
Cloud User <centos@ip-172-31-11-0.eu-west-2.compute.internal>
2021-06-08 11:44:58 +0000

    Exercise 4 solution

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Terence Tan
+// Date: 8 June 2021
 //
 //  Description: In this exercise, you need to design a LED based lighting solution, 
 //  following the diagram provided in the exercises documentation. The lights change 
@@ -15,4 +15,55 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module LED (
+	input clk,
+	input rst,
+	input button,
+	output [2:0] colour
+	);
+
+	reg [2:0] colour;
+
+
+	// User logic
+
+	always @(posedge clk) begin
+		if (rst) begin
+			colour<=0;
+		end
+		else begin
+
+		if (colour==0) begin
+			colour <= colour + 1;
+		end
+
+		else if (colour==7) begin
+			colour <= 1;
+		end
+
+		else begin
+			if (colour==6) begin
+				if (button) begin
+					colour<=1;
+				end
+				else begin
+				colour<=colour;
+				end
+			end
+			else begin
+				if (button) begin
+					colour <=colour+1;
+				end
+				else begin
+					colour<=colour;
+				end
+			end
+		end
+		end
+	end
+
+endmodule
+
+				
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -1,9 +1,74 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Terence Tan
+// Date: 8 June 2021
 //
 // Description: A testbench module to test Ex4 - Dynamic LED lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+module top_tb(
+	);
+
+	// Parameters
+	parameter CLK_PERIOD = 10;
+
+	// Wires and registers
+	wire [2:0] colour;
+	reg clk;
+	reg rst;
+	reg button;
+	reg error;
+
+	// clock generation
+	initial
+	begin	
+		clk = 1'b0;
+		forever
+			# (CLK_PERIOD/2) clk=~clk;
+	end
+
+// User logic
+	initial begin
+		error = 0;
+	end
+
+	initial begin
+		button =1;
+			forever
+				# (CLK_PERIOD*2) button=~button;
+	end
+
+	initial begin
+		rst = 1;
+			forever
+				# (CLK_PERIOD*20) rst=~rst;
+	end
+
+	//forever begin
+	//# (CLK_PERIOD - 6)
+	//if (colour==7) begin
+		//$display("***TEST FAILED! 
+// Finish test, check for success
+	initial begin
+		#600
+		if (error==0)
+			$display("***TEST PASSED! HOORAY! :) ***");
+		$finish;
+	end
+
+// Instantiate counter module
+
+	LED top(
+	.clk (clk),
+	.rst (rst),
+	.button (button),
+	.colour (colour)
+	);
+
+endmodule
+
+
+
 
