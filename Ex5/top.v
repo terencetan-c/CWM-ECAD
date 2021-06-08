0e05efdcd1571fd26726b949d7ee10b95b2dbb15
Cloud User <centos@ip-172-31-11-0.eu-west-2.compute.internal>
2021-06-08 12:06:11 +0000

    Exercise 5 solution

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name: Terence Tan
+// Date: 8 June 2021
 //
 //  Description: In this exercise, you need to an air conditioning control system
 //  According to the state diagram provided in the exercise.
@@ -12,4 +12,19 @@
 //  outputs:
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module Air_con (
+	input clk,
+	input [4:0] temperature,
+	output heating,
+	output cooling
+	);
+
+	reg [4:0] temperature;
+
+
+	//User logic
+	
+	always @(posedge clk) begin
+		
