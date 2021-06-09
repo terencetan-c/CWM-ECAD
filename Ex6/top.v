fc333f800281e6f6bc378b4618ebaf62cafce33f
Cloud User <centos@ip-172-31-11-0.eu-west-2.compute.internal>
2021-06-09 11:57:09 +0000

    Exercise 6 solution

--- Ex6/top.v
+++ Ex6/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #6 - RGB Colour Converter
-// Student Name:
-// Date: 
+// Student Name: Terence Tan
+// Date: 9 June 2021
 //
 //
 //  Description: In this exercise, you need to design a memory with 8 entries, 
@@ -14,4 +14,37 @@
 //           rgb [23:0]
 //
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module RGB_converter(
+	input clk,
+	input [2:0] colour,
+	input enable,
+	output [23:0] rgb
+	);
+	
+
+
+	wire douta, rgb;
+	wire enable,ena;
+	wire clk,clka;
+	wire colour, addra;
+
+	assign wea = 0;
+	assign dina=0;
+
+
+	
+
+
+	RGB_Converter your_instance_name (
+  		.clka(clk),    // input wire clka
+  		.ena(enable),      // input wire ena
+  		.wea(wea),      // input wire [0 : 0] wea
+  		.addra(colour),  // input wire [2 : 0] addra
+  		.dina(dina),    // input wire [23 : 0] dina
+  		.douta(rgb)  // output wire [23 : 0] douta
+	);
+
+
+endmodule
--- Ex6/top_tb.v
+++ Ex6/top_tb.v
@@ -1,9 +1,70 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #6 - RGB Colour Converter
-// Student Name:
-// Date: 
+// Student Name: Terence Tan
+// Date: 9 June 2021
 //
 // Description: A testbench module to test Ex6 - RGB Colour Converter
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+	);
+
+	// Parameters
+	parameter CLK_PERIOD = 10;
+
+	// Wires and registers
+	wire [2:0] colour;
+	wire [23:0] rgb;
+	reg clk;
+	wire enable;
+	reg error;
+
+
+
+	// Clock generation
+	initial
+	begin
+		clk = 1'b0;
+		forever
+			# (CLK_PERIOD/2) clk=~clk;
+	end
+
+	// User logic
+		initial begin
+			error = 0;
+		end
+		
+
+		
+		//forever begin
+		//# (CLK_PERIOD-6)
+		
+		//If both heating and cooling are 'ON', which is forbidden, test failed
+		//if ((heating)&&(cooling)) begin
+		//	error = 1;
+		//	$display("***TEST FAILED! Heating and Cooling are both 'ON'!! :( ***");
+		//$finish;
+		//end
+		//end
+		//end
+
+	// Finish test, check for success
+		initial begin
+			#600
+			if (error==0)
+				$display("***TEST PASSED! HIP HIP HOORAY!! :) ***");
+			$finish;
+		end
+
+	// Instantiate counter module
+		
+	RGB_converter top(
+		.clk (clk),
+		.colour (colour),
+		.enable (enable),
+		.rgb (rgb)
+		);
+
+endmodule
